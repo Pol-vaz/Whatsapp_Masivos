3ae2f42d87ce1d253b0befead26383888ab2167d
pol <51190617+Pol-vaz@users.noreply.github.com>
2020-02-24 13:32:03 +0100

    Update Whatsapp_Grupos.py

--- Whatsapp_Grupos.py
+++ Whatsapp_Grupos.py
@@ -6,40 +6,46 @@ from selenium.webdriver.common.by import By
 import time
 import sys
 
-def Whatsapp_Grupos_Masivos(objetivo):
-    print('\n\n####################################  Mandador de mensajes Whatsapp by pol and the Bros  ##############################\n')
-    num=int(input('\nIntroduce el número de veces que quieres mandar el mensaje: '))
-    # Aquí escribe tu mensaje de puto enfermo
-    texto=input('\nIntroduce tu mensaje: ')
+class Whatsapp_Grupos:
 
-    eleccion=input(f'\n\nSe va a enviar el siguiente mensaje:\n\n         "{texto}"   {num} veces al siguiente grupo o contacto:\n\n        {objetivo}\n\nSi todo es correcto pulse "s", en caso contario pulse "n": ')
+    def whatsapp(self):
+        self.datos()
+        
+        self.eleccion=input(f'\n\nSe va a enviar el siguiente mensaje:\n\n         "{self.texto}"   {self.num} veces al siguiente grupo o contacto:\n\n        {self.objetivo}\n\nSi todo es correcto pulse "s", en caso contario pulse "n": ')
+        if self.eleccion is 's':
+            self.driver()
+            self.sending()
+        else:
+            print('\nHasta la vista.\n')
 
-    if eleccion is 's':
-        #Simons, aqui reemplaza la ruta donde este tu chromedriver (En este caso es para Linux)
-        conductor = webdriver.Chrome(r'/home/pol/Escritorio/Whatsapps_Masivos/chromedriver')
+    def datos(self):
+        print('\n\n####################################  Mandador de mensajes Whatsapp by pol and the Bros  ##############################\n')
+        nombre=input('\nIntroduce el nombre del grupo o contacto al que quieres mandar el mensaje: ')
+        self.objetivo = '''"'''+nombre+'''"'''
+        self.num=int(input('\nIntroduce el número de veces que quieres mandar el mensaje: '))
+        # Aquí escribe tu mensaje de puto enfermo
+        self.texto=input('\nIntroduce tu mensaje: ')
 
-        conductor.get("https://web.whatsapp.com/") #Se abre aquí whatsapp Web en chrome
-
-        espera = WebDriverWait(conductor, 600)   #Espera de 600 mili
-        print ("Espera unos segundos mientras surge la magia.")
-
-        #Repite el bucle de mandado de mensajes las veces que desees
-        for i in range(num):
+    def driver(self):
+        self.conductor = webdriver.Chrome(r'/home/pol/Escritorio/Whatsapps_Masivos/chromedriver')
+        self.conductor.get("https://web.whatsapp.com/") #Se abre aquí whatsapp Web en chrome
+        self.espera = WebDriverWait(self.conductor, 600)   #Espera de 600 mili
+        
+    def sending(self):
+        for i in range(self.num):
             #Busca el grupo o el contacto según el nombre dado
-            x_arg = '//span[contains(@title,' + objetivo + ')]'
-            titulo_grupo = espera.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+            x_arg = '//span[contains(@title,' + self.objetivo + ')]'
+            titulo_grupo = self.espera.until(EC.presence_of_element_located((By.XPATH, x_arg)))
             titulo_grupo.click() 
-            mensaje = conductor.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+            mensaje = self.conductor.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
             #Mando el mensaje al grupo o contacto
-            mensaje.send_keys(texto)
-            enviar = conductor.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+            mensaje.send_keys(self.texto)
+            enviar = self.conductor.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
             enviar.click()
-        conductor.close()
+        self.conductor.close()
+        print ("\nEspera unos segundos mientras surge la magia.↓\n")
         print('\nListo')
-    else:
-        print('\nHasta la vista,mariquita\n')
 
-
-#AQUI CAMBIA EL NOMBRE DEL GRUPO O EL CONTACTO.
-objetivo ='"Dee Lujoo!!"'
-Whatsapp_Grupos_Masivos(objetivo)
+       
+enviador = Whatsapp_Grupos()
+enviador.whatsapp()
